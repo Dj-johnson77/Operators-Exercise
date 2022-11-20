9edf09b6757d0b4fa1fc7f522e0f4436f10b699b
Donald Johnson <donaldrjohnson66@gmail.com>
2022-11-19 20:48:35 -0600

    Finished exercises and input a comment on what I think the answer of exercise 3 might be.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -3,8 +3,50 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   
+            //Exercise 1
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            //Exercise 2
+            Console.WriteLine("what is the radius of your circle?");
+
+            var userInput = (Console.ReadLine());
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+            /* Exercise 3? 
+             * k=16 */
+
+            
+
+
         }
+
+      
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
+
+            
+
+
+        } 
+        
+
+
+
+
+        
+
+        
     }
 }
